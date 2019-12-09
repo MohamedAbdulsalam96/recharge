c938da070d62d281aa8b36d982e5fc21338a061d
jiloysss <jangeles@bai.ph>
2019-12-09 14:46:37 +0800

    Purchase Target Report With Pandas

--- recharge/recharge/report/purchase_target_report/purchase_target_report.py
+++ recharge/recharge/report/purchase_target_report/purchase_target_report.py
@@ -3,165 +3,56 @@
 
 from __future__ import unicode_literals
 import frappe
+import pandas as pd
 from datetime import *
 import calendar
 from time import strptime
-
 def execute(filters=None):
 	columns, data = [], []
-	add_columns(columns,"Date", "date")
-	if filters.get("warehouse_target"):
-		warehouses = frappe.get_list("Warehouse", fields=["*"])
-		warehouse_target = frappe.get_doc("Warehouse Target",filters.get("warehouse_target")).__dict__
-		defaults = frappe.get_single("Global Defaults").__dict__
-		month = warehouse_target['month']
-		year = warehouse_target['year']
-		total_target_amount = warehouse_target['total_target_amount']
-		month_int = int(strptime(month, '%B').tm_mon)
-		_to = int(str(calendar.monthrange(int(year), month_int))[4] + str(calendar.monthrange(int(year), month_int))[5])
-		for day in range(1,_to):
-			date_string = str(month) + " " + str(day) + " " + str(year)
-			date = datetime.strptime(date_string, '%B %d %Y')
+	columns.append({"label": "Date","width": 120,"fieldname": "date","fieldtype": "Data"})
 
-			total_purchase = 0
-			purchases = {}
-			purchases["date"] = day
-			if total_target_amount > 0:
-				for i in warehouses:
-					total = get_total_purchases(i.name,date.date(),warehouse_target)
-					if total > 0 and not check_if_column_labels(columns,i.warehouse_name):
-						total_purchase += total
-						add_columns(columns,i.warehouse_name,i.warehouse_name.lower().replace(" ","").replace("-","_"))
-						purchases[i.warehouse_name.lower().replace(" ", "").replace("-", "_")] = total
-					elif total > 0:
-						total_purchase += total
-						purchases[i.warehouse_name.lower().replace(" ","").replace("-","_")] = total
-				purchases["total_purchase"] = total_purchase
-				purchases["target_balance"] = total_target_amount - total_purchase
-				purchases["complete_total_purchase"] = float(round((total_purchase / total_target_amount),2))
-				purchases["complete_target_balance"] = float(round(((total_target_amount - total_purchase) / total_target_amount),2))
-				data.append(purchases)
+	warehouse_targets = get_sales_invoice(filters)  # coming from frappe.db.sql in get_stock_ledger_entries()
+	colnames = [key for key in warehouse_targets[0].keys()]  # create list of columns used in creating dataframe
+	print(colnames)
+	df = pd.DataFrame.from_records(warehouse_targets,
+								   columns=colnames)  # this is key to get the data from frappe.db.sql loaded correctly.
+	print(df)
+	pvt = pd.pivot_table(
+		df,
+		values='total',
+		index=['posting_date'],
+		columns='set_warehouse',
+		fill_value=0
+	)
 
-		add_columns(columns, "Total Purchase", "total_purchase")
-		add_columns(columns, "Target Balance", "target_balance")
-		add_columns(columns, "% Complete", "complete_total_purchase")
-		add_columns(columns, "% Complete", "complete_target_balance")
-		total_computations(columns,warehouses,data, warehouse_target,filters)
-		return columns, data
-def get_total_purchases(warehouse, date, warehouse_target):
+	data = pvt.reset_index().values.tolist()  # reset the index and create a list for use in report.
+
+	columns += pvt.columns.values.tolist()# create the list of dynamic columns added to the previously defined static columns
+
+	print(data)
+	return columns, data
+
+def get_sales_invoice(filters):
+	warehouse_target = frappe.get_doc("Warehouse Target", filters.get("warehouse_target")).__dict__
 	brand = warehouse_target['brand']
 	item_group = warehouse_target['item_group']
-	total_that_will_be_returned = 0
-	pi = frappe.db.sql(
-		""" SELECT name, total FROM `tabPurchase Invoice` WHERE set_warehouse=%s and posting_date=%s and status != %s""",
-		(warehouse, date, "Cancelled"), as_dict=True)
-	print(warehouse)
-	print("PURCHASE INVOICE")
-	print(pi)
-	for i in pi:
-		valid_pi = check_valid_pi(i.name,brand,item_group)
-		if valid_pi:
-			total_that_will_be_returned += float(i.total)
-	return total_that_will_be_returned
 
-def check_valid_pi(pi_name,brand,item_group):
-	pi_details = frappe.get_list("Purchase Invoice Item", filters={"parent": pi_name}, fields=["*"])
-	for i in pi_details:
-		get_item = frappe.get_list("Item", filters={"name": i.item_code, "brand": brand, "item_group": item_group}, fields=["*"])
-		if len(get_item) == 0:
-			return False
-	return True
-def add_columns(columns,label,fieldname):
-	columns.append({
-		"label": label,
-		"width": 120,
-		"fieldname": fieldname,
-		"fieldtype": "Currency" if fieldname != "date" else "Data"})
+	month_int = int(strptime(filters.get("warehouse_target").split()[0], '%B').tm_mon)
 
-def check_if_column_labels(columns,label):
-	for i in columns:
-		if i["label"] == label:
-			return True
-	return False
-
-def check_date_in_holiday(date,defaults):
-	holiday_list = frappe.get_list("Holiday", filters={"parent": defaults["default_holidays"]}, fields=["holiday_date"])
-	for i in holiday_list:
-		if i.holiday_date == date:
-			return True
-	return False
-
-#TOTAL COMPUTATIONS and DIFFERENCES
-def total_computations(columns,warehouses,data, warehouse_target,filters):
-	warehouse_totals = {}
-	warehouse_totals_difference = {}
-	commissions = {}
-	commissions_amount = {}
-	target_over_number_of_working_days = {}
-	sum_multiplied_to_number_of_working_days = {}
-	sum_multiplied_to_number_of_working_days_minus_sum = {}
+	warehouse_targets = frappe.db.sql(
+		""" SELECT PI.posting_date, SUM(PI.total) as total, PI.set_warehouse FROM 
+			(SELECT PI2.posting_date, PI2.status, PI2.total, PI2.set_warehouse FROM `tabPurchase Invoice` AS PI2  
+			INNER JOIN `tabPurchase Invoice Item` ON PI2.name = `tabPurchase Invoice Item`.parent  
+			WHERE EXISTS (SELECT * FROM `tabItem` 
+			WHERE `tabItem`.name = `tabPurchase Invoice Item`.item_code 
+			and brand=%s 
+			and item_group=%s) 
+			and PI2.status != %s 
+			and MONTH(posting_date) = %s
+			and YEAR(posting_date) = %s 
+			and `tabPurchase Invoice Item`.idx = 1 AND EXISTS (SELECT * FROM `tabWarehouse Target Details` 
+			WHERE `tabWarehouse Target Details`.warehouse = PI2.set_warehouse) ) as PI 
+			GROUP BY posting_date, set_warehouse""",(brand, item_group,"Cancelled", month_int, "2019"), as_dict=True)
 
 
-	warehouse_totals["date"] = "Totals"
-	for i in warehouses:
-		if check_if_column_labels(columns,i.warehouse_name):
-			sum_per_warehouse = sum(d[i.warehouse_name.lower().replace(" ","").replace("-","_")] if i.warehouse_name.lower().replace(" ","").replace("-","_") in d else 0 for d in data if d)
-			get_target_amount_value = get_target_amount(warehouse_target["warehouse_target_details"],i.warehouse_name.lower().replace(" ","").replace("-","_"))
-			no_of_working_days = frappe.get_doc("Warehouse Target",filters.get("warehouse_target")).__dict__['number_of_working_days']
-			difference = sum_per_warehouse - get_target_amount_value if get_target_amount_value > 0 else 0
-			commission_value = commission_check(sum_per_warehouse,get_target_amount_value,filters)[0]
-			commission_amount_value = commission_check(sum_per_warehouse,get_target_amount_value,filters)[1]
-			warehouse_totals[i.warehouse_name.lower().replace(" ","").replace("-","_")] = sum_per_warehouse
-			warehouse_totals_difference[i.warehouse_name.lower().replace(" ","").replace("-","_")] = difference
-			commissions[i.warehouse_name.lower().replace(" ","").replace("-","_")] = float(round(commission_value,2))
-			commissions_amount[i.warehouse_name.lower().replace(" ","").replace("-","_")] = float(round(commission_amount_value,2))
-			target_over_number_of_working_days[i.warehouse_name.lower().replace(" ","").replace("-","_")] = float(round(get_target_amount_value / no_of_working_days,2))
-			sum_multiplied_to_number_of_working_days[i.warehouse_name.lower().replace(" ","").replace("-","_")] = float(round(((get_target_amount_value / no_of_working_days) * no_of_working_days),2))
-			sum_multiplied_to_number_of_working_days_minus_sum[i.warehouse_name.lower().replace(" ","").replace("-","_")] = float(round((get_target_amount_value / no_of_working_days) * no_of_working_days - sum_per_warehouse,2))
-
-	data.append({})
-	data.append(warehouse_totals)
-	data.append({})
-	data.append(warehouse_totals_difference)
-	data.append({})
-	data.append(commissions_amount)
-	data.append(commissions)
-	data.append({})
-	data.append(target_over_number_of_working_days)
-	data.append(sum_multiplied_to_number_of_working_days)
-	data.append(sum_multiplied_to_number_of_working_days_minus_sum)
-	data.append({})
-
-
-
-	return warehouse_totals
-
-#GET TARGET AMOUNT
-def get_target_amount(warehouse_target_details, warehouse_name):
-	for i in warehouse_target_details:
-		data = i.__dict__
-		get_warehouse_name = frappe.get_value("Warehouse", data["warehouse"], "warehouse_name")
-		change_name = get_warehouse_name.lower().replace(" ","").replace("-","_")
-		if change_name == warehouse_name and data["target_amount"] > 0:
-			return data["target_amount"]
-	return 0
-
-#COMPUTE COMMISION
-def commission_check(sum_per_warehouse,target_amount_value,filters):
-	percentage_bonus_return = 0
-	warehouse_target = frappe.get_doc("Warehouse Target", filters.get("warehouse_target")).__dict__
-
-	year = warehouse_target['year']
-
-	get_monthly_commisions = frappe.get_list("Monthly Bonus", filters={"parent": "BONUSES " + str(year)}, fields=["percentage_target", "percentage_bonus"], order_by="percentage_bonus")
-
-	if target_amount_value == 0:
-		return 0,percentage_bonus_return
-
-	percentage = (sum_per_warehouse / target_amount_value) * 100
-
-	for i in get_monthly_commisions:
-		if percentage >= int(i.percentage_target):
-			percentage_bonus_return = sum_per_warehouse * (i.percentage_bonus/100)
-
-	return percentage,percentage_bonus_return
+	return warehouse_targets
