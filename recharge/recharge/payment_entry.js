36f7ae81d6d35ec588e2bcc6e99fd0dcfbc523cd
Info Aakvatech <info@aakvatech.com>
2019-12-21 12:12:36 +0000

    fix: Removed payment entry js as csftz manages it

--- recharge/recharge/payment_entry.js
+++
@@ -1,7 +0,0 @@
-frappe.ui.form.on("Payment Entry", "payment_type", function(frm) {
-    if (cur_frm.doc.payment_type == "Pay") {
-	cur_frm.set_value("party_type", "Supplier");
-    } else if (cur_frm.doc.payment_type == "Receive") {
-	cur_frm.set_value("party_type", "Customer");
-    }
-});
