1b3df1bd7e536d742f3ede0c19537194545e4505
Info Aakvatech <info@aakvatech.com>
2020-08-12 04:36:07 +0000

    fix: Unnecessary console logs

--- recharge/recharge/sales_invoice.js
+++ recharge/recharge/sales_invoice.js
@@ -2,10 +2,10 @@ frappe.ui.form.on('Sales Invoice Item', {
 	item_code:function(frm,cdt,cdn){
 		var row=locals[cdt][cdn];
 		var doc=locals['Sales Invoice'][row.parent]
-		console.log(doc);
+		// console.log(doc);
 		for(var item in doc.items){
 			if(doc.items[item].item_code==row.item_code && doc.items[item].start_series==row.start_series && doc.items[item].end_series==row.end_series){
-				console.log("true")
+				// console.log("true")
 				if(doc.items[item].name!=row.name){
 					frappe.model.set_value(cdt,cdn,"item_code","")
 					frappe.throw("Item, Start Series and End Series Already Selected");
